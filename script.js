68ace26d2fa1a1bfe91098dd52b135caf50bcf85
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-26 11:39:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputs = document.querySelectorAll('.controls input');
 
+
     function handleUpdate() {
       const suffix = this.dataset.sizing || '';
       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
@@ -7,3 +8,11 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+const bac10 = document.getElementById("back10");
+bac10.addEventListener("click",()=>{
+	
+})
+const for25 = document.getElementById("forward25");
+for25.addEventListener("click", ()=>{
+	
+})
